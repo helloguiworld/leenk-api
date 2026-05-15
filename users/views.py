217371c305aa1f859ecf60b27b89b640f6a8879f
Guilherme Feitosa <feitosa.guilhermef@gmail.com>
2026-05-15 00:21:44 -0300

    Add API status endpoint and URL configuration

--- users/views.py
+++ users/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
-# Create your views here.
+class APIStatusView(APIView):
+    """
+    Simple view to check if the API is running correctly.
+    """
+    def get(self, request):
+        return Response({
+            "status": "online",
+            "message": "Leenk API is up and running!",
+            "version": "v1"
+        }, status=status.HTTP_200_OK)
