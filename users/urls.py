58ea400ba3ce9a4d347bfd6cf2cbdec6c9264085
Guilherme Feitosa <feitosa.guilhermef@gmail.com>
2026-05-15 00:53:43 -0300

    UI: Clean up users API root description

--- users/urls.py
+++ users/urls.py
@@ -2,6 +2,8 @@ from rest_framework.routers import DefaultRouter
 from .views import StatusViewSet
 
 router = DefaultRouter()
+router.APIRootView.name = "Users"
+router.APIRootView.__doc__ = None
 router.register(r'status', StatusViewSet, basename='status')
 
 urlpatterns = router.urls
